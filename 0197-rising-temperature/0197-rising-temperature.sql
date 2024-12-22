225f10afd869abcf5974611b9017847141937a61
Pavan kumar <115355309+pavankumar6174q@users.noreply.github.com>
2024-12-22 15:06:21 +0530

    Time: 651 ms (24.41%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,6 +2,6 @@
 select w1.id from
 weather w1
 join weather w2
-on w1.recordDate = DATE_ADD(w2.recordDate, interval 1 day)
+on subdate(w1.recordDate, 1) = w2.recordDate 
 where w1.temperature > w2.temperature;
 
