55db61258df19bd4070fd47f0c2c829b9df4069a
Pavan kumar <115355309+pavankumar6174q@users.noreply.github.com>
2025-02-07 20:34:29 +0530

    Time: 901 ms (37.96%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-select w1.id from
-weather w1
-join weather w2
-on subdate(w1.recordDate, 1) = w2.recordDate 
-where w1.temperature > w2.temperature;
+select w1.id from weather w1
+join weather w2 
+on subdate(w1.recordDate, 1) = w2.recordDate
+where w1.temperature > w2.temperature
+
 
